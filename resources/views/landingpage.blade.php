46fdd5c1216214215476d3984b35fac6def0d841
Anugrah Tejo Maliki <tejomaliki1@email.com>
2026-05-04 14:37:24 +0700

    add cache busting for landingpagenew.css

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Brilliant English Course</title>
 
-    <link rel="stylesheet" href="{{ asset('css/landingpagenew.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/landingpagenew.css') }}?v={{ filemtime(public_path('css/landingpagenew.css')) }}">
     <script src="{{ asset('js/gallery.js') }}"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
